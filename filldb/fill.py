25cf65b741be5dc0286f244cb753d3ec0d904d42
Shane Barratt <stbarratt@gmail.com>
2015-05-21 20:15:38 -0400

    working

--- filldb/fill.py
+++ filldb/fill.py
@@ -4,7 +4,7 @@ import json
 import random
 
 
-account_id = '555bed95a520e036e52b2680'
+account_id = '555bed95a520e036e52b2787'
 api_key = '4baacf4ff89dcf34346063a26c8b7d0b'
 
 link = 'http://api.reimaginebanking.com:8080/accounts/'+account_id+'/bills?key='+api_key
@@ -21,10 +21,53 @@ def make_payment(payee, date, amount):
 
 def main():
 	companies = ['Apple', 'Seven-Eleven', 'Uber', 'Starbucks', 'Amazon.com','Costco', 'The Local Pub', 'Hooters', 'Venmo.com', 'H&M', 'UPS', 'United']
-	
-	months = ['0'+str(i) for i in range(1,6)]
-	days = [str(i) for i in range(1,31)]
-	years = ['2015']
+
+	year = '2015'
+
+	for i in range(1,32):
+		date = year+'-'+'1'+'-'+str(i)
+		payee = companies[random.randint(0,len(companies)-1)]
+		if payee not in ['Seven-Eleven', 'Uber', 'Starbucks', 'The Local Pub', 'Hooters', 'UPS']:
+			amount = random.randint(50,500)
+		else:
+			amount = random.randint(5,50)
+		make_payment(payee, date, amount)
+
+	for i in range(1,29):
+		date = year+'-'+'2'+'-'+str(i)
+		payee = companies[random.randint(0,len(companies)-1)]
+		if payee not in ['Seven-Eleven', 'Uber', 'Starbucks', 'The Local Pub', 'Hooters', 'UPS']:
+			amount = random.randint(50,500)
+		else:
+			amount = random.randint(5,50)
+		make_payment(payee, date, amount)
+	print "2"
+	for i in range(1,32):
+		date = year+'-'+'3'+'-'+str(i)
+		payee = companies[random.randint(0,len(companies)-1)]
+		if payee not in ['Seven-Eleven', 'Uber', 'Starbucks', 'The Local Pub', 'Hooters', 'UPS']:
+			amount = random.randint(50,500)
+		else:
+			amount = random.randint(5,50)
+		make_payment(payee, date, amount)
+	print "3"
+	for i in range(1,31):
+		date = year+'-'+'4'+'-'+str(i)
+		payee = companies[random.randint(0,len(companies)-1)]
+		if payee not in ['Seven-Eleven', 'Uber', 'Starbucks', 'The Local Pub', 'Hooters', 'UPS']:
+			amount = random.randint(50,500)
+		else:
+			amount = random.randint(5,50)
+		make_payment(payee, date, amount)
+	print "4"
+	for i in range(1,23):
+		date = year+'-'+'5'+'-'+str(i)
+		payee = companies[random.randint(0,len(companies)-1)]
+		if payee not in ['Seven-Eleven', 'Uber', 'Starbucks', 'The Local Pub', 'Hooters', 'UPS']:
+			amount = random.randint(50,500)
+		else:
+			amount = random.randint(5,50)
+		make_payment(payee, date, amount)
 
 	# for month in months:
 	# 	for day in days:
--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,17 @@
-$("#slider").noUiSlider({
-	start: [20, 80],
-	connect: true,
-	range: {
-		'min': 0,
-		'max': 100
-	}
-});
+// $("#slider").noUiSlider({
+// 	start: [90, 130],
+// 	connect: true,
+// 	range: {
+// 		'min': 0,
+// 		'max': 141
+// 	}
+// });
 
-$('#hi').innerHTML = $('#slider').Link('lower').to($('#slider-value'));
+var items = {'Uber':400, 'Safeway':20};
+require(['account', 'atm', 'branch', 'customer', 'deposit', 'transaction', 'withdrawal', 'bills']);
+bills = require('bills');
+var apikey = '4baacf4ff89dcf34346063a26c8b7d0b';
+items = billDemo(apikey, bills, '2015-01-01','2015-05-22');
 
 function findUseful(bills) {
   //Filters out irrelevant properties within bill
@@ -56,29 +60,29 @@ function sortByAmount(sortedValidBills) {
 
 var app = angular.module('myApp',[]);
 
-$(function(){
-	require(['account', 'atm', 'branch', 'customer', 'deposit', 'transaction', 'withdrawal', 'bills'], function (account, atm, branch, customer, deposit, transaction, withdrawal, bills) {
-		var apikey = '4baacf4ff89dcf34346063a26c8b7d0b';
-		//var custAccount = account.initWithKey(apikey);
-		//var customer = custAccount.getAll()[0];
-		//accountDemo(apikey, account);
-		//atmDemo(apikey, atm);
-		billDemo(apikey, bills);
-		//branchDemo(apikey, branch);
-		// customerDemo(apikey, customer);
-		//depositDemo(apikey, deposit);	
-		//transactionDemo(apikey, account, transaction);
-		//withdrawalDemo(apikey, withdrawal);
-	});
-});
+// $(function(){
+// 	require(['account', 'atm', 'branch', 'customer', 'deposit', 'transaction', 'withdrawal', 'bills'], function (account, atm, branch, customer, deposit, transaction, withdrawal, bills) {
+// 		var apikey = '4baacf4ff89dcf34346063a26c8b7d0b';
+// 		//var custAccount = account.initWithKey(apikey);
+// 		//var customer = custAccount.getAll()[0];
+// 		//accountDemo(apikey, account);
+// 		//atmDemo(apikey, atm);
+// 		billDemo(apikey, bills);
+// 		//branchDemo(apikey, branch);
+// 		// customerDemo(apikey, customer);
+// 		//depositDemo(apikey, deposit);	
+// 		//transactionDemo(apikey, account, transaction);
+// 		//withdrawalDemo(apikey, withdrawal);
+// 	});
+// });
 
-function billDemo (apikey, bills) {
+function billDemo (apikey, bills, start, end) {
 	var bm = bills.initWithKey(apikey);
 	// console.log("[Customer Account Bills] : " + JSON.stringify(bm.getAllBillsByAccountId("555bed95a520e036e52b2680")));
-	var bills = bm.getAllBillsByAccountId("555bed95a520e036e52b2680");
+	var bills = bm.getAllBillsByAccountId("555bed95a520e036e52b2787");
 	// console.log("[Customer Account Bills] : " + bm.getAllBillsByAccountId("555bed95a520e036e52b2680"));
 	// var result = bm.getAllBillsByAccountId("555bed95a520e036e52b2680");
-	var filtered = filterDate("2015-05-21", "2015-05-29", bills);
+	var filtered = filterDate(start, end, bills);
 	var l = new Object();
 	for (i in filtered) {
 		payment = filtered[i];
@@ -99,10 +103,46 @@ function billDemo (apikey, bills) {
 	// console.log(l);
 	// console.log(JSON.stringify(filtered));
 }
+
+$("#range_22").ionRangeSlider({
+	type: "double",
+    min: new Date("January 1 2015").getTime(),
+    max: new Date("May 22 2015").getTime(),
+    prettify: function (num) {
+    	return new Date(num).toDateString();
+	},
+	onFinish: function (data) {
+		function formatDate(d) {
+		    var month = '' + (d.getMonth() + 1),
+		        day = '' + d.getDate(),
+		        year = d.getFullYear();
+
+		    if (month.length < 2) month = '0' + month;
+		    if (day.length < 2) day = '0' + day;
+
+		    return [year, month, day].join('-');
+		}
+		var start = formatDate(new Date(data.from));
+		var end = formatDate(new Date(data.to));
+		// require(['account', 'atm', 'branch', 'customer', 'deposit', 'transaction', 'withdrawal', 'bills'])
+	    bills = require('bills');
+		var apikey = '4baacf4ff89dcf34346063a26c8b7d0b';
+		window.rootScope.items = billDemo(apikey, bills, start, end);
+		window.rootScope.$apply();
+	}
+});
 	
 app.controller('ListController', function($scope) {
-    require(['account', 'atm', 'branch', 'customer', 'deposit', 'transaction', 'withdrawal', 'bills'])
+	window.rootScope = $scope;
+	items = {'Uber':400, 'Safeway':20};
+    require(['account', 'atm', 'branch', 'customer', 'deposit', 'transaction', 'withdrawal', 'bills']);
     bills = require('bills');
 	var apikey = '4baacf4ff89dcf34346063a26c8b7d0b';
-	$scope.items = billDemo(apikey, bills);
+	$scope.items = billDemo(apikey, bills, '2015-01-01','2015-05-22');
 });
+
+app.controller('sliderController', function($scope) {
+	var lower_value = document.getElementById('lower_value').innerHTML;
+	var upper_value = document.getElementById('upper_value').innerHTML;
+});
+
